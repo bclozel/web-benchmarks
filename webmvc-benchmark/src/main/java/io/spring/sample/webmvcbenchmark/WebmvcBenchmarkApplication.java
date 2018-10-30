d8a54716bc3123d40ac3236fd53e0fabb4985948
Brian Clozel <bclozel@pivotal.io>
2018-10-30 15:27:54 +0100

    Add WebMVC sample project

---
+++ webmvc-benchmark/src/main/java/io/spring/sample/webmvcbenchmark/WebmvcBenchmarkApplication.java
@@ -0,0 +1,12 @@
+package io.spring.sample.webmvcbenchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmvcBenchmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmvcBenchmarkApplication.class, args);
+	}
+}
