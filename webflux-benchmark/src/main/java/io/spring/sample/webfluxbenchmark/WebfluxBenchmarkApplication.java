1f6dd6b0038ebb604dc46b8e94110fbe9a2b522d
Brian Clozel <bclozel@pivotal.io>
2018-10-31 11:50:52 +0100

    Add WebFlux sample project

---
+++ webflux-benchmark/src/main/java/io/spring/sample/webfluxbenchmark/WebfluxBenchmarkApplication.java
@@ -0,0 +1,12 @@
+package io.spring.sample.webfluxbenchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxBenchmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxBenchmarkApplication.class, args);
+	}
+}
---
+++ webflux-benchmark/src/test/java/io/spring/sample/webfluxbenchmark/WebfluxBenchmarkApplicationTests.java
@@ -0,0 +1,16 @@
+package io.spring.sample.webfluxbenchmark;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebfluxBenchmarkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
